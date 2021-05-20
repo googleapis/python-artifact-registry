4e0198a03236b15d2aa205bd62999a573aba8a26
shabirmean <7249208+Shabirmean@users.noreply.github.com>
2021-05-19 20:19:00 -0400

    chore: revert changes causing broken tests

--- google/cloud/artifactregistry_v1beta2/types/file.py
+++ google/cloud/artifactregistry_v1beta2/types/file.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import proto  # type: ignore
-
 
 from google.protobuf import timestamp_pb2  # type: ignore
 
